749779c214f964612f9710d94ffb64a4b2349419
Josh Holmer <jholmer.in@gmail.com>
2022-09-25 11:27:49 -0400

    oiadsffoi

--- src/zimg/colorspace/operation_impl.cpp
+++ src/zimg/colorspace/operation_impl.cpp
@@ -7,6 +7,7 @@
 #include "operation.h"
 #include <algorithm>
 #include <cfloat>
+#include <csignal>
 #include <iostream>
 
 #if defined(ZIMG_X86)
@@ -367,6 +368,7 @@ create_cl_yuv_to_rgb_operation(const ColorspaceDefinition &in,
   fprintf(stderr, "%.6f %.6f %.6f\n", m[0][0], m[0][1], m[0][2]);
   fprintf(stderr, "%.6f %.6f %.6f\n", m[1][0], m[1][1], m[1][2]);
   fprintf(stderr, "%.6f %.6f %.6f\n", m[2][0], m[2][1], m[2][2]);
+  std::raise(SIGINT);
 
   return std::make_unique<CLToRGBOperationC>(func.to_gamma, func.to_linear,
                                              m[0][0], m[0][1], m[0][2],
